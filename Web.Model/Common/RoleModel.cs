e1bf3fd22c5ecebd169f9fe466d021ef42fa2971
muhammadzeehlx <94970989+muhammadzeehlx@users.noreply.github.com>
2021-11-26 17:11:53 +0500

    Roles Api Changes

---
+++ Web.Model/Common/RoleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Model.Common
+{
+    class RoleModel
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+        public string RoleDiscrimination { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
