9ee475c0c2b21ac63388ae879bb9f28cd30ee901
tayyabarif42 <tayyab@hawklogix.com>
2021-12-06 16:53:00 +0500

    get component access param formate changed

--- Web.API/Controllers/AdminController.cs
+++ Web.API/Controllers/AdminController.cs
@@ -274,7 +274,7 @@ namespace Web.API.Controllers
         }
 
         [Description("Get All Components By UserRole Id")]
-        [HttpGet("admin/GetComponentsByUserRoleId/{roleId}/{userId}")]
+        [HttpGet("admin/GetComponentsByUserRoleId")]
         public async Task<BaseResponse> GetComponentsByUserRoleId(int roleId, int userId)
         {
             try
