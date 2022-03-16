29793c52dfd00754173d490d8b5245bced043c5e
Zaibyteror <94899984+Zaibyteror@users.noreply.github.com>
2022-03-16 19:09:29 +0500

    Call log VM

---
+++ Web.Data/Models/CallLogVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Data.Models
+{
+    public class CallLogVM
+    {
+        public int CallLogId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Duration { get; set; }
+        public string Direction { get; set; }
+        public string CallStatus { get; set; }
+        public string ToPhoneNumber { get; set; }
+        public string ToName { get; set; }
+        public string FromPhoneNumber { get; set; }
+        public string FromName { get; set; }
+        public string CallSid { get; set; }
+        public string ParentCallSid { get; set; }
+        public string RecordingName { get; set; }
+        public bool IsRecorded { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
