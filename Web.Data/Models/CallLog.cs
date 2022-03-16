4f6c3f2f7077ba8bb60177d96551babb83ee4a16
muhammadzeehlx <94970989+muhammadzeehlx@users.noreply.github.com>
2022-03-16 17:58:05 +0500

    scaffolded

--- Web.Data/Models/CallLog.cs
+++ Web.Data/Models/CallLog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 #nullable disable
 
@@ -6,31 +7,20 @@ namespace Web.Data.Models
 {
     public partial class CallLog
     {
-        public int Id { get; set; }
-        public DateTime? StartTime { get; set; }
+        public int CallLogId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public string Duration { get; set; }
-        public string Type { get; set; }
         public string Direction { get; set; }
-        public string Action { get; set; }
-        public string Result { get; set; }
-        public string TophoneNumber { get; set; }
-        public string Toname { get; set; }
-        public string FromphoneNumber { get; set; }
-        public string Fromname { get; set; }
-        public string Fromlocation { get; set; }
-        public string Reason { get; set; }
-        public string ReasonDescription { get; set; }
-        public string RecordingUri { get; set; }
-        public string RecordingType { get; set; }
-        public string RecordingContentUri { get; set; }
-        public DateTime? Cd { get; set; }
-        public bool? Taskgenerated { get; set; }
-        public string RecordingName { get; set; }
-        public string Isrecorded { get; set; }
-        public string Callsource { get; set; }
-        public string ConferenceName { get; set; }
-        public string RecordingDateTime { get; set; }
-        public string PatientId { get; set; }
+        public string CallStatus { get; set; }
+        public string ToPhoneNumber { get; set; }
+        public string ToName { get; set; }
+        public string FromPhoneNumber { get; set; }
+        public string FromName { get; set; }
+        public string CallSid { get; set; }
         public string ParentCallSid { get; set; }
+        public string RecordingName { get; set; }
+        public bool IsRecorded { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
