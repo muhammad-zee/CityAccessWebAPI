318febf9164919da33fa700ab8ffc2f60e716e7b
muhammadzeehlx <94970989+muhammadzeehlx@users.noreply.github.com>
2022-04-19 12:52:37 +0500

    scaffolded

---
+++ Web.Data/Models/CallReservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Web.Data.Models
+{
+    public partial class CallReservation
+    {
+        public int ReservationId { get; set; }
+        public int QueueIdFk { get; set; }
+        public int ReservationStatus { get; set; }
+        public int ReservationAssignedTo { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+        public virtual CallQueue QueueIdFkNavigation { get; set; }
+    }
+}
