506057c3782811d927a071f3a1366d45d3b4f713
muhammadzeehlx <94970989+muhammadzeehlx@users.noreply.github.com>
2022-06-23 16:37:22 +0500

    twilio automation service added

---
+++ MDRoute.TwilioAutomationService/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwilioAutomationService
+{
+    public static class Configuration
+    {
+
+        public static string Base_Url = "https://5353-202-166-174-174.ngrok.io/";
+    }
+}
---
+++ MDRoute.TwilioAutomationService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TwilioAutomationService;
+
+namespace MDRoute.TwilioAutomationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutomationService.TriggerQueues();
+        }
+    }
+}
